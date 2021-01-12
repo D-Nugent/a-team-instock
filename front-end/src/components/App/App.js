8394f606fdb9e7b9878de29361de06b9e3481081
David Nugent <davienugent@hotmail.com>
2021-01-11 17:30:53 -0800

    Secondary Folder Structure Update - Page Structure Determined and Mock Routes Set-Up to be edited Later - Typography files imported - still to be applied in scss

--- front-end/src/components/App/App.js
+++ front-end/src/components/App/App.js
@@ -3,21 +3,46 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      {/* Header Component Will Go Here */}
+      <Header/>
+      <Switch>
+        {/* 1.1 */}
+        <Route exact path="/warehouse-main" render={(routeProps) =>
+          <Home {...routeProps}/>}/>
+          {/* On warehouse main there will be links to take you to specific pages for warehouse details, i.e.
+           http://localhost:3000/warehouse-main/33sdfs8d7s9df, this will conditionally generate content based on props (think video reel) */}
+        {/* 4.1 */}
+        <Route path="/inventory-main" render={(routeProps) =>
+          <Home {...routeProps}/>}/>
+          {/* On warehouse main there will be links to take you to specific pages for warehouse details, i.e.
+           http://localhost:3000/warehouse-main/33sdfs8d7s9df, this will conditionally generate content based on props (think video reel) */}
+          {/* ---------------------------------------------- */}
+          {/* 2.0 */}
+          <Route path="/inventory-main/:id" render={(routeProps) =>
+          <ContentDetails {...routeProps}/>}/>
+          {/* 5.0 */}
+         <Route path="/warehouse-main/:id" render={(routeProps) =>
+          <ContentDetails {...routeProps}/>}/>
+          {/* ---------------------------------------------- */}
+         {/* 3.1 */}
+         <Route path="/warehouse-main/:id/edit" render={(routeProps) =>
+          <ContentEdit {...routeProps}/>}/>
+          {/* 6.1 */}
+         <Route path="/inventory-main/:id/edit" render={(routeProps) =>
+          <ContentEdit {...routeProps}/>}/>
+          {/* ---------------------------------------------- */}
+          {/* 3.2 */}
+         <Route path="/inventory-main/new-item" render={(routeProps) =>
+          <ContentNew {...routeProps}/>}/>
+          {/* 6.2 */}
+         <Route path="/warehouse-main/new-item" render={(routeProps) =>
+          <ContentNew {...routeProps}/>}/>
+          {/* ---------------------------------------------- */}
+
+        <Route component={PageNotFound}/>
+      </Switch>
     </div>
   );
 }
