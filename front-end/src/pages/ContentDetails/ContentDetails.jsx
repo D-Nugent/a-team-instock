f79c23525df58d28e3924e53b9312c350c988468
David Nugent <davienugent@hotmail.com>
2021-01-13 02:17:58 -0800

    Inventory Item Detail Component Commit & Debug - Component constructed in jsx and scss - Responsive and Adaptive Design added for each breakpoint - Some minor adjustments to header - Created new icon for 'edit' as not included in assets package - Adjusted positioning of copyright text - should now be accurate across all pages - Fixed error with App.js routes, 'exact' added to allow routes with the same name - Added typography reset and imported font-styles - Added additional variable colors to accommodate transparency - Component currently designed with placeholders, needing back-end

--- front-end/src/pages/ContentDetails/ContentDetails.jsx
+++ front-end/src/pages/ContentDetails/ContentDetails.jsx
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import returnIcon from '../../assets/icons/arrow_back-24px.svg';
+import editOffsetIcon from '../../assets/icons/edit-offset-24px.svg';
+import './ContentDetails.scss';
 
 export class ContentDetails extends Component {
-    render() {
-        return (
-            <div>
-                <p>ContentDetails is used for Warehouse Details(2.0) and Inventory Item Details(5.0)</p>
+  render() {
+    return (
+      <div className="content">
+        <div className="content__heading">
+          <div className="content__heading-nav">
+            <img src={returnIcon} alt="back arrow" className="content__heading-nav-return"/>
+            <h1 className="content__heading-nav-title">Television</h1>
+          </div>
+          <div className="content__heading-edit">
+            <img src={editOffsetIcon} alt="edit icon" className="content__heading-edit-icon"/>
+          </div>
+        </div>
+        <div className="content__detail">
+          <div className="content__detail-type">
+            <h4 className="content__detail-type-descrheading">ITEM DESCRIPTION:</h4>
+            <p className="content__detail-type-descrvalue">This 50", 4K LED TV proides a crystal-clear picture and vivid colors.</p>
+            <h4 className="content__detail-type-catheading">CATEGORY:</h4>
+            <p className="content__detail-type-catvalue">Electronics</p>
+          </div>
+          <div className="content__detail-specs">
+            <div className="content__detail-specs-stock">
+              <div className="content__detail-specs-stock-status">
+                <h4 className="content__detail-specs-stock-status-heading">STATUS:</h4>
+                <div className="content__detail-specs-stock-status-value">IN STOCK</div>
+              </div>
+              <div className="content__detail-specs-stock-quantity">
+                <h4 className="content__detail-specs-stock-quantity-heading">QUANTITY:</h4>
+                <p className="content__detail-specs-stock-quantity-value">500</p>
+              </div>
             </div>
-        )
-    }
+            <div className="content__detail-specs-warehouse">
+              <h4 className="content__detail-specs-warehouse-heading">WAREHOUSE:</h4>
+              <p className="content__detail-specs-warehouse-value">Manhattan</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ContentDetails
