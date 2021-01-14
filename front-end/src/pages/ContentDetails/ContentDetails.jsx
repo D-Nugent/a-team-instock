c0446ca1a806bb45f9014f9809eaa79fe1a82abc
David Nugent <davienugent@hotmail.com>
2021-01-13 22:55:10 -0800

    Header of Warehouse Details page created - Header for Warehouse Details page created - Conditional content and styling added dependent on url path - Still using placeholder data for warehouse which will require backend - Renamed path url's in App.js i.e. warehouse-main > warehouse - List of inventory items will be dependent on ticket LR-2 so will need to return to ensure that this is implemented. - Adjustments made to resp and adaptive styling incl relative units - Functionality of edit button and back arrow added

--- front-end/src/pages/ContentDetails/ContentDetails.jsx
+++ front-end/src/pages/ContentDetails/ContentDetails.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 import returnIcon from '../../assets/icons/arrow_back-24px.svg';
 import editOffsetIcon from '../../assets/icons/edit-offset-24px.svg';
 import './ContentDetails.scss';
 
 export class ContentDetails extends Component {
   state={
-    currentInventoryItem: [],
+    currentItem: [],
+    currentRoute: this.props.routeType,
   }
 
-componentDidMount() {
-  let itemID = this.props.match.params.id
-  axios
-  .get(`${process.env.REACT_APP_API_URL}/inventory/${itemID}`)
-  .then((response) => {
-    console.log(response.data);
-    this.setState({
-      currentInventoryItem: response.data
+  
+  componentDidMount() {
+  let testInvID = '4b6a7565-077e-4595-a317-c53095fd5dad'
+  let testWarID = 'ade0a47b-cee6-4693-b4cd-a7e6cb25f4b7'
+
+  console.log(this.state);
+  console.log(this.props);
+  console.log(this.props.match.path);
+  let itemID = this.props.match.url
+    axios
+    .get(`${process.env.REACT_APP_API_URL}${itemID}`)
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        currentItem: response.data
+      })
     })
-  })
 }
 
   render() {
@@ -26,37 +35,61 @@ componentDidMount() {
       <div className="content">
         <div className="content__heading">
           <div className="content__heading-nav">
-            <img src={returnIcon} alt="back arrow" className="content__heading-nav-return"/>
-            <h1 className="content__heading-nav-title">{this.state.currentInventoryItem.itemName}</h1>
+            <img src={returnIcon} alt="back arrow" className="content__heading-nav-return" onClick={()=> {this.props.history.push(`${this.state.currentRoute === 'inventory'?"/inventory":"/warehouse"}`)}}/>
+            <h1 className="content__heading-nav-title">{this.state.currentItem.itemName}King West</h1>
           </div>
-          <div className="content__heading-edit">
+          <Link to={`/${this.state.currentRoute}/${this.props.match.params.id}/edit`} className="content__heading-edit">
             <img src={editOffsetIcon} alt="edit icon" className="content__heading-edit-icon"/>
             <p className="content__heading-edit-text">Edit</p>
-          </div>
+          </Link>
         </div>
         <div className="content__detail">
-          <div className="content__detail-type">
-            <h4 className="content__detail-type-descrheading">ITEM DESCRIPTION:</h4>
-            <p className="content__detail-type-descrvalue">{this.state.currentInventoryItem.description}</p>
-            <h4 className="content__detail-type-catheading">CATEGORY:</h4>
-            <p className="content__detail-type-catvalue">{this.state.currentInventoryItem.category}</p>
+        {this.state.currentRoute === 'inventory' ?
+         <div className="content__detail-type">
+            <h4 className="content__detail-type-heading">ITEM DESCRIPTION:</h4>
+            <p className="content__detail-type-value">{this.state.currentItem.description}</p>
+            <h4 className="content__detail-type-heading">CATEGORY:</h4>
+            <p className="content__detail-type-value">{this.state.currentItem.category}</p>
           </div>
-          <div className="content__detail-specs">
-            <div className="content__detail-specs-stock">
-              <div className="content__detail-specs-stock-status">
-                <h4 className="content__detail-specs-stock-status-heading">STATUS:</h4>
-                <div className={`content__detail-specs-stock-status-value${this.state.currentInventoryItem.quantity === 0?" --out-of-stock":""}`}>{this.state.currentInventoryItem.status}</div>
+          :
+         <div className="content__detail-type --warehouse">
+            <h4 className="content__detail-type-heading">WAREHOUSE ADDRESS:</h4>
+            <address className="content__detail-type-value">{this.state.currentItem.address}469 King Street West,<br/>Toronto, CAN</address>
+          </div>
+          }
+          {this.state.currentRoute === 'inventory' ?
+           <div className={"content__detail-specs"}>
+            <div className="content__detail-specs-container">
+              <div className="content__detail-specs-container-status">
+                <h4 className="content__detail-specs-container-status-heading">STATUS:</h4>
+                <div className={`content__detail-specs-container-status-value${this.state.currentItem.quantity === 0?" --out-of-stock":""}`}>{this.state.currentItem.status}</div>
               </div>
-              <div className="content__detail-specs-stock-quantity">
-                <h4 className="content__detail-specs-stock-quantity-heading">QUANTITY:</h4>
-                <p className="content__detail-specs-stock-quantity-value">{this.state.currentInventoryItem.quantity}</p>
+              <div className="content__detail-specs-container-quantity">
+                <h4 className="content__detail-specs-container-quantity-heading">QUANTITY:</h4>
+                <p className="content__detail-specs-container-quantity-value">{this.state.currentItem.quantity}</p>
               </div>
             </div>
             <div className="content__detail-specs-warehouse">
               <h4 className="content__detail-specs-warehouse-heading">WAREHOUSE:</h4>
-              <p className="content__detail-specs-warehouse-value">{this.state.currentInventoryItem.warehouseName}</p>
+              <p className="content__detail-specs-warehouse-value">{this.state.currentItem.warehouseName}</p>
             </div>
           </div>
+          :
+           <div className="content__detail-specs --warehouse">
+            <div className="content__detail-specs-container">
+              <div className="content__detail-specs-container-contact">
+                <h4 className="content__detail-specs-container-contact-heading">CONTACT NAME:</h4>
+                <div className="content__detail-specs-container-contact-name">{this.state.currentItem.name}Graeme Lyon</div>
+                <div className="content__detail-specs-container-contact-position">{this.state.currentItem.position}Warehouse Manager</div>
+              </div>
+              <div className="content__detail-specs-container-communication">
+                <h4 className="content__detail-specs-container-communication-heading">CONTACT INFORMATION:</h4>
+                <p className="content__detail-specs-container-communication-phone">{this.state.currentItem.phone}+1 (647)504-0911</p>
+                <p className="content__detail-specs-container-communication-email">{this.state.currentItem.email}glyon@instock.com</p>
+              </div>
+            </div>
+          </div>
+          }
         </div>
       </div>
     )
