60eb36269febe74ba22683aa339fe10fd140d3b6
Ryan Doubinin <ryan_doubinin@hotmail.com>
2021-01-15 11:01:06 -0700

    OLL20Q5LR-8-RD: Set up back-end API to POST a new inventory item. Added to inventoryRoutes.js. Ready for peer review.

--- back-end/api/inventoryRoutes.js
+++ back-end/api/inventoryRoutes.js
@@ -3,14 +3,30 @@ const app = express();
 const router = express.Router();
 const inventories = require("./routes/inventories.json");
 
-router.route("/")
-.get((req, res) => {
+router.route("/").get((req, res) => {
   res.status(200).send(inventories);
 });
 
-router.route("/:id")
-.get((req, res) => {
-  res.status(200).send(inventories.filter(item => item.id === req.params.id).shift())
-})
+router.route("/:id").get((req, res) => {
+  res.status(200).send(inventories.filter((item) => item.id === req.params.id).shift());
+});
+
+router.route("/new").post((req, res) => {
+  const { item, description, category, status, quantity, warehouse } = req.body;
+
+  let inventoryNew = {
+    id: uuidv4(),
+    item,
+    description,
+    category,
+    status,
+    quantity,
+    warehouse,
+  };
+
+  inventories.push(inventoryNew);
+
+  res.status(201).send(inventoryNew);
+});
 
 module.exports = router;
