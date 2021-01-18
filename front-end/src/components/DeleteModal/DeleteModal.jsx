911a22808b2bdcd8cc2e6d24916f1c893673da74
David Nugent <davienugent@hotmail.com>
2021-01-17 17:25:30 -0800

    Page and Component Merging & Delete Modal Funct - Adjustments to Home.jsx to remove conflicts and errors in redirect - Conditional rendering re-added to prevent undefined data errors - DeleteModal call and Component Import complete

--- front-end/src/components/DeleteModal/DeleteModal.jsx
+++ front-end/src/components/DeleteModal/DeleteModal.jsx
@@ -2,25 +2,49 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./DeleteModal.scss";
 import Close from "../../assets/icons/close-24px.svg";
-
-function DeleteModal() {
-  return (
-    <section className="delete">
-      <div className="delete-modal">
-        <div className="delete__close-button-container">
-          <img className="delete__close-button" src={Close} alt="letter x to signify close button" />
+export class DeleteModal extends Component {
+  state = {
+    route: this.props.routeProps.match.path,
+    deleteTarget: this.props.deleteTarget,
+  };
+  componentDidMount() {
+    this.state.route.path === "/warehouse/:id"
+      ? axios.get(`${process.env.REACT_APP_API_URL}/inventory/${this.state.deleteTarget}`)
+      : axios.get(`${process.env.REACT_APP_API_URL}${this.state.route.path}/${this.state.deleteTarget}`);
+  }
+  deleteHandler = () => {
+    this.state.route === "/warehouse/:id"
+      ? axios.delete(`${process.env.REACT_APP_API_URL}/inventory/${this.state.deleteTarget}`)
+      : axios.delete(`${process.env.REACT_APP_API_URL}${this.state.route}/${this.state.deleteTarget}`);
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <section className="delete">
+        <div className="delete-modal">
+          <div className="delete__close-button-container">
+            <img className="delete__close-button" src={Close} alt="letter x to signify close button" />
+          </div>
+          {this.state.route === "/inventory" ? (
+            <div className="delete__content-container">
+              <h1 className="delete__header">Delete inventory item?</h1>
+              <p className="delete__body">Please confirm that you'd like to delete Television from the inventory list. You won't be able to undo this action.</p>
+            </div>
+          ) : (
+            <div className="delete__content-container">
+              <h1 className="delete__header">Delete King West warehouse?</h1>
+              <p className="delete__body">Please confirm that you'd like to delete King West from the list of warehouses. You won't be able to undo this action.</p>
+            </div>
+          )}
+          <div className="delete__button-container">
+            <button className="delete__button-cancel">Cancel</button>
+            <button className="delete__button-delete" onSubmit={() => this.deleteHandler()}>
+              Delete
+            </button>
+          </div>
         </div>
-        <div className="delete__content-container">
-          <h1 className="delete__header">Delete Television inventory item?</h1>
-          <p className="delete__body">Please confirm that you'd like to delete Television from the inventory list. You won't be able to undo this action.</p>
-        </div>
-        <div className="delete__button-container">
-          <button className="delete__button-cancel">Cancel</button>
-          <button className="delete__button-delete">Delete</button>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
-
 export default DeleteModal;
